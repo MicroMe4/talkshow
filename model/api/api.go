f66ca8c111ec5ac6f13f8977da2c8662e5ee6095
microme4 <aidoyle@MasterMind.localdomain>
2020-02-12 13:47:05 +0800

    Key Append

--- model/api/api.go
+++ model/api/api.go
@@ -1,6 +1,9 @@
 package api
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
 //Key 登陆客户端控制系统
 type Key struct {
-	AppKey `json:"appid" bson:"appid" xml:"appid"`
+	ID     primitive.ObjectID `bson:"_id" json:"_id"`
+	AppKey string             `json:"appid" bson:"appid" xml:"appid"`
 }
