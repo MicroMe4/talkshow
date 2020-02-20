e495acf7b1507b094d96a7640ff56f2c7b2d385a
microme4 <aidoyle@MasterMind.localdomain>
2020-02-21 01:59:58 +0800

    add server restriction model

--- model/api/api.go
+++ model/api/api.go
@@ -2,7 +2,7 @@ package api
 
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
-//Key 登陆客户端控制系统
+//Key 可允许登陆到服务器的ID
 type Key struct {
 	ID     primitive.ObjectID `bson:"_id" json:"_id"`
 	AppKey string             `json:"appid" bson:"appid" xml:"appid"`
